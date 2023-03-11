03a8659cc174b142d8c792710cb714d845b4f40e
Applepie46 <emmanuel.cadavid46@gmail.com>
2023-03-10 19:04:52 -0500

    [Emmanuel][v-1.0][feat] Add pop up to ejs view  template and styles

--- public/js/calculateValoration.js
+++ public/js/calculateValoration.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Elements
     const heighttInput = document.querySelector(".height-input");
     const weightInput = document.querySelector(".weight-input");
-
     const radioBtnYes = document.getElementById("radio-btn-yes");
     const radioBtnNo = document.getElementById("radio-btn-no");
     const imcInput = document.querySelector(".IMC-input");
-    const resultInput = document.getElementById("valoration");
+    const resultInput = document.getElementById("estimation");
 
+    // Adds a listener to input elements to see if the value is changing
     radioBtnYes.addEventListener("change", valoration);
     radioBtnNo.addEventListener("change", valoration);
     imcInput.addEventListener("input", valoration);
@@ -22,12 +23,12 @@ document.addEventListener("DOMContentLoaded", () => {
     function valoration() {
         if (imcInput.value) {
             if (radioBtnNo.checked && imcInput.value == "Peso normal o adecuado") {
-                resultInput.value = "ACTO"
+                resultInput.value = "ACTO";
             } else if (radioBtnYes.checked && imcInput.value != "Peso normal o adecuado") {
-                resultInput.value = "NO ACTO"
+                resultInput.value = "NO ACTO";
             }
         } else {
-            resultInput.value = "Ingresa los campos de altura y peso"
+            resultInput.placeholder = "Ingresa los campos de altura y peso"
         }
     }
 
